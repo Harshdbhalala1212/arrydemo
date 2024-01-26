427ed7f4d1e1f0d2ff1d2f707d729c3ab7ad930a
Harsh Bhalala <harshdbhalala1212@gmail.com>
2024-01-26 16:43:34 -0500

    added class

---
+++ src/PartTimeclass.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author harsh
+ */
+public class PartTimeclass extends student{
+    private int NumCourses;
+
+    public int getNumCourses() {
+        return NumCourses;
+    }
+
+    public PartTimeclass(int NumCourses) {
+        this.NumCourses = NumCourses;
+    }
+
+    public void setNumCourses(int NumCourses) {
+        this.NumCourses = NumCourses;
+    }
+ 
+}
